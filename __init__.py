7adf6fac66bf96b740cd8129253b2ca5dd7ba764
Amit Kulkarni <kulkarni.am@husky.neu.edu>
2017-03-07 16:07:32 -0500

    Allows loading more news

--- __init__.py
+++ __init__.py
@@ -12,6 +12,12 @@ headlines_only_flag = results.headlines
 
 def run():
     isn_object = InShortNews(debug=False, headlines_only = headlines_only_flag)
+    while 1:
+        load_more_news = raw_input("Load more news [y/n]: ")
+        if load_more_news == 'y' or load_more_news == 'Y' or load_more_news == "":
+            isn_object.get_more_news()
+        else:
+            break
 
 if __name__ == "__main__":
     run()
--- inshortnews_API.py
+++ inshortnews_API.py
@@ -2,6 +2,7 @@
 import requests
 import logging
 from lxml import html
+from lxml import etree
 import textwrap
 import json
 
@@ -29,9 +30,23 @@ class InShortNews(object):
         "Accept-Encoding": "gzip,deflate,sdch,br",
         "Accept-Language": "en-US,en;q=0.8,ms;q=0.6",
     }
+    more_news_request_headers = {
+        "Host": host,
+        "Connection": "keep-alive",
+        "User-Agent": user_agent,
+        "Origin": "https://"+host,
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept": "*/*",
+        "DNT": "1",
+        "Referer": "https://www.inshorts.com/en/read",
+        "Accept-Encoding": "gzip,deflate,sdch,br",
+        "Accept-Language": "en-US,en;q=0.8"
+    }
     headlines_only = False
     NEWS_HEADLINE_LENGTH = 80
     NEWS_DETAILS_LENGTH = 80
+    min_news_id = ""
+    print_count = 1
 
     def __init__(self, debug=False, headlines_only=False):
         '''Initialize variables required for all instances'''
@@ -51,6 +66,41 @@ class InShortNews(object):
         for line in textwrap.wrap(news, char_count_per_line, break_long_words=False):
             print line
 
+    def parse_news(self, news_articles):
+        list_of_homepage_news = [
+            {
+                'headline': news.xpath('.//span[@itemprop="headline"]')[0].xpath('.//text()')[0],
+                'headline_description': news.xpath('.//div[@itemprop="articleBody"]')[0].xpath('.//text()')[0],
+                'headline_link': news.xpath('.//a[@class="source"]')[0].get('href').strip(),
+                'headline_id': news_articles[0].xpath('.//a[@class="source"]')[0].get('href').strip().split('/', 1)[
+                    1].split('/').pop()
+            }
+            for news in news_articles
+            ]
+        return list_of_homepage_news
+
+    def print_news(self, home_page_news_items):
+        if(self.headlines_only):
+            self.print_headlines_only(home_page_news_items)
+        else:
+            self.print_all_news(home_page_news_items)
+
+    def print_headlines_only(self,list_of_news):
+        for news_item in list_of_news:
+            # InShortNews.formatted_news(news_item['headline'], True)
+            print "["+str(self.print_count)+"] "+news_item['headline']
+            self.print_count += 1
+
+    def print_all_news(self, list_of_news):
+        print "--------------------------------------------------------------------------------"
+        for news_item in list_of_news:
+            InShortNews.formatted_news(news_item['headline'], True)
+            print "--------------------------------------------------------------------------------"
+            if not self.headlines_only:
+                InShortNews.formatted_news(news_item['headline_description'], False)
+                InShortNews.formatted_news(news_item['headline_link'], True)
+                print
+
     def initialize_session(self):
         # Now, isn_request has cookies with it and we need not attach it every time henceforth
         self.isn_request = requests.Session()
@@ -61,19 +111,24 @@ class InShortNews(object):
             exit(1)
         tree = html.fromstring(home_page_response.content)
         news_articles = tree.xpath('.//div[@itemtype="http://schema.org/NewsArticle"]');
-        list_of_homepage_news = [
-            {
-                'headline'              : news.xpath('.//span[@itemprop="headline"]')[0].xpath('.//text()')[0],
-                'headline_description'  : news.xpath('.//div[@itemprop="articleBody"]')[0].xpath('.//text()')[0],
-                'headline_link'         : news.xpath('.//a[@class="source"]')[0].get('href').strip()
-            }
-                for news in news_articles
-            ]
-        print "--------------------------------------------------------------------------------"
-        for news_item in list_of_homepage_news:
-            InShortNews.formatted_news(news_item['headline'], True)
-            print "--------------------------------------------------------------------------------"
-            if not self.headlines_only:
-                InShortNews.formatted_news(news_item['headline_description'], False)
-                InShortNews.formatted_news(news_item['headline_link'], True)
-                print
+        home_page_news_items = self.parse_news(news_articles)
+        # Finding this looks really manual but we are not left with any other option
+        # Using the news_id of the last article for the new loads isn't helping
+        # So, let's just work with the way their HTML works
+        # CAUTION: Required debugging (Information isn't available)
+        self.min_news_id = tree.xpath('.//script[contains(text(),"min_news_id")]')[0].xpath('.//text()')[0].strip().split(';',1)[0].split("\"")[1]
+        self.print_news(home_page_news_items)
+
+    def get_more_news(self):
+        form_data = {
+            "category":"",
+            "news_offset": self.min_news_id
+        }
+        more_news_response = self.isn_request.post(self.isn_more_URL,
+                                                   data=form_data,
+                                                   headers=self.more_news_request_headers)
+        # Update the latest news received
+        self.min_news_id = more_news_response.json()['min_news_id']
+        news_articles = html.fromstring(more_news_response.json()['html'].strip())
+        more_news_articles = self.parse_news(news_articles)
+        self.print_news(more_news_articles)
